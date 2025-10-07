c2a112d067e7f80e47263fdf5fa406bccbc21d8e
Pasindu86 <pasindupamudith@gmail.com>
2025-10-08 02:34:45 +0530

    feat/adding loging and regitrastion

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AuctionSystem.Api.Models;
+
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace AuctionSystem.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+     
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // User constraints
+            modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+
+        
+        }
+    }
+}
